cdbdf7afc976c953cc8c6f7d519d8c29ce7554e5
Krystel Rodriguez Castejon <krystel.dev@gmail.com>
2021-09-28 23:30:15 +0200

    Prioridad ens feedbacks + remove

--- js/checkout.js
+++ js/checkout.js
@@ -1,15 +1,17 @@
 // Get the input fields
-var password = document.querySelector(".password");
-var phone = document.querySelector('.phone');
-var finalname = document.querySelector('.name'); //Nom
-var inputForm = document.querySelectorAll('.input'); // Todas las entradas del formulario
-var lastName = document.querySelector('#lastName'); //Cognom
-var email = document.querySelector('#email'); //Correo electrónico
+var password = document.querySelector(".password"); // Contrasenya
+var phone = document.querySelector('.phone'); // telf
+var finalName = document.querySelector('.name'); //Nom
+var lastName = document.getElementById('lastName'); //Cognom
+var email = document.getElementById('email'); //Correo electrónico
+var adress = document.getElementById('adress'); // adreasa
 
 // Get the error elements
 var errorPassword = document.getElementById("errorPassword");
 var errorName = document.getElementById('errorName');  
 var errorPhone = document.getElementById('errorPhone');  
+var errorGeneral = document.getElementById('feedbackError');
+
 
 // Exercise 9
 function validate() {
@@ -20,51 +22,106 @@ function validate() {
 
    //Crea un "espia/escoltador" pel que pasi pel formulari. Quant es vagi al submit fes l'event..
    form.addEventListener('submit', (event) => {
-    inputForm.forEach(element => { //mira cada element del llistat de inputForm...
-        //...Si:
-        //- Tots els camps són obligatoris.
-        if (element.value == '') { // si algún està incomplet no cumplirà el primer requisit.
-            event.preventDefault(); // Event anular envío de fomulario
-            element.classList.add("is-invalid"); // añadir una clase
-        }
-        //- Tots els camps han de tenir almenys 3 caràcters.
-        else if (element.value.length < 3) {
-            event.preventDefault(); // Event anular envío de fomulario
-            element.classList.add("is-invalid"); // añadir una clase
-        } 
+       //Condicionals:
 
-        /** Información de las cuatro siguentes condiciones:
-         * https://lineadecodigo.com/javascript/validar-que-el-texto-introducido-es-un-numero/
-         * expresión regular + funcion .test()
-         **/
-        //- El nom ha de contenir només lletres /^[a-zA-Z]+$.
-        if (!(/^[a-zA-Z]+$/.test(finalname.value))) {
+        /** Información de las expresión regular + funcion .test()
+        * https://lineadecodigo.com/javascript/validar-que-el-texto-introducido-es-un-numero/
+        **/
+
+    //Validar Nom:
+        if (!validarTamano(finalName)) {
             event.preventDefault(); // Event anular envío de fomulario
-            finalname.classList.add("is-invalid"); // añadir una clase
+            var errorVacio = finalName.nextElementSibling;
+            errorVacio.innerHTML = 'Campo obligatorio con mínimo 3 caracteres';
+            finalName.classList.add("is-invalid");
+        } else if (!(/^[a-zA-Z]+$/.test(finalName.value))) {
+            event.preventDefault(); // Event anular envío de fomulario
+            var errorName = finalName.nextElementSibling;
+            errorName.innerHTML = 'Debe contener solo letras';
+            finalName.classList.add("is-invalid"); // añadir una clase
+       } else {
+          finalName.classList.remove("is-invalid");
         }
-        //- El cognom ha de contenir només lletres /^[a-zA-Z]+$.
-        if (!(/^[a-zA-Z]+$/.test(lastName.value))) {
+
+    //Validar Cognom:
+        if (!validarTamano(lastName)) {
             event.preventDefault(); // Event anular envío de fomulario
+            var errorVacio = lastName.nextElementSibling;
+            errorVacio.innerHTML = 'Campo obligatorio con mínimo 3 caracteres';
+            lastName.classList.add("is-invalid");
+        //Campo obligatorio
+        } else if (!(/^[a-zA-Z]+$/.test(lastName.value))) {
+            event.preventDefault(); // Event anular envío de fomulario
+            var errorName = lastName.nextElementSibling;
+            errorName.innerHTML = 'Debe contener solo letras';
             lastName.classList.add("is-invalid"); // añadir una clase
-       }
-        //- El telèfon ha de contenir només números /^([0-9])*$/.
-        if (/^([0-9])*$/.test(phone.value)){
-            event.preventDefault(); // Event anular envío de fomulario
-            phone.classList.add("is-invalid"); // añadir una clase
+        } else {
+            lastName.classList.remove("is-invalid");
         }
-        //- La contrasenya ha d'incloure números i lletres. ^(?=.?\d)(?=.?[a-zA-Z])[a-zA-Z\d]+$
-        if (!(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{3,}$/.test(password.value))){
-            event.preventDefault(); // Event anular envío de fomulario
-            password.classList.add("is-invalid"); // añadir una clase
-        }
-        //- L'email ha de tenir format d'email. 
-        //Info: http://w3.unpocodetodo.info/utiles/regex-ejemplos.php?type=email
-        if (!(/^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/).test(password.value)){
-            event.preventDefault(); // Event anular envío de fomulario
-            email.classList.add("is-invalid"); // añadir una clase
-        }
-    });
 
-   });
+        //Validar telèfon:
+        if (!validarTamano(phone)) {
+            event.preventDefault(); // Event anular envío de fomulario
+            var errorVacio = phone.nextElementSibling;
+            errorVacio.innerHTML = 'Campo obligatorio con mínimo 3 caracteres';
+            phone.classList.add("is-invalid");
+        } else if (!(/^([0-9])*$/.test(phone.value))){
+            event.preventDefault(); // Event anular envío de fomulario
+            var errorPhone = phone.nextElementSibling;
+            errorPhone.innerHTML = 'Debe contener solo números';
+            phone.classList.add("is-invalid"); // añadir una clase
+        } else {
+            phone.classList.remove("is-invalid");
+        }
+
+        //Validar Password:
+        if (!validarTamano(password)) {
+            event.preventDefault(); // Event anular envío de fomulario
+            var errorVacio = password.nextElementSibling;
+            errorVacio.innerHTML = 'Campo obligatorio con mínimo 3 caracteres';
+            password.classList.add("is-invalid");
+        } else if (!(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{3,}$/.test(password.value))){
+            event.preventDefault(); // Event anular envío de fomulario
+            var errorPassword = password.nextElementSibling;
+            errorPassword.innerHTML =  'Debe contener mínimo una letra y un número.';
+            password.classList.add("is-invalid"); // añadir una clase
+        } else {
+            password.classList.remove("is-invalid");
+        }
+
+        //Validar Email:
+        if (!validarTamano(email)) {
+            event.preventDefault(); // Event anular envío de fomulario
+            var errorVacio = email.nextElementSibling;
+            errorVacio.innerHTML = 'Campo obligatorio con mínimo 3 caracteres';
+            email.classList.add("is-invalid");
+        } else if (!(/^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/).test(email.value)){
+            event.preventDefault(); // Event anular envío de fomulario
+            var errorEmail = email.nextElementSibling;
+            errorEmail.innerHTML = 'Debe tener un formato de e-mail válido. algo@algo.dominio';
+            email.classList.add("is-invalid"); // añadir una clase
+        } else {
+            email.classList.remove("is-invalid");
+        }
+
+        //Validar adress:
+        if (!validarTamano(adress)) {
+            event.preventDefault(); // Event anular envío de fomulario
+            var errorVacio = adress.nextElementSibling;
+            errorVacio.innerHTML = 'Campo obligatorio con mínimo 3 caracteres';
+            adress.classList.add("is-invalid");
+        } else {
+            adress.classList.remove("is-invalid");
+        }
+
+   }, {once: true});
     
+}
+
+function validarTamano (input) {
+    if (input.value.length < 3) {
+        return false;
+     } else {
+         return true
+     }
 }
